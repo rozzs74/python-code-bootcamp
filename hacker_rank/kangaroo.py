28a09c2c075bd315c61ad067e9d19648a24cd982
Royce <rozzs74@gmail.com>
2020-04-14 20:14:37 +0800

    Solved kangaroo challenge

--- hacker_rank/kangaroo.py
+++ hacker_rank/kangaroo.py
@@ -2,25 +2,25 @@
 
 
 def kangaroo(x1, v1, x2, v2):
-    k1_jump_result = x1
-    k2_jump_result = x2
-    i = 0
-    if x2 > x1 or v2 > v1:
+    if x2 > x1 and v2 > v1: 
         return "NO"
-    else :
-        while True:
-            x1 = x1 + v1
-            x2 = x2 + v2
-            i += 1
-            if x1 == x2:
-                return "YES"
-
-
+    i = 0
+    while 1 < 10000:
+        x1 = x1 + v1
+        # print(f"x1 {x1}")
+        x2 = x2 + v2
+        i += 1
+        if x1 == x2:
+            return "YES"
+        if i == 10000 and x1 != x2:
+            return "NO"
+    return "NO"
 # result = kangaroo(0, 3, 4, 2)
 # result = kangaroo(2, 1, 1, 2)
-# result = kangaroo(0, 2, 5, 3)
-result = kangaroo(21, 6, 47, 3)
+result = kangaroo(0, 2, 5, 3)
+# result = kangaroo(21, 6, 47, 3) # NO
 
+# result = kangaroo(0, 3, 4,2)
 # 21 6 47 3
 print(result)
 # 0 2 5 3
