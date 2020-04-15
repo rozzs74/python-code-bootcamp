eb0ffdf1bcee2adac8d9bc73828ab09d9c8030cf
Royce <rozzs74@gmail.com>
2020-04-16 07:43:17 +0800

    solved two sets

--- hacker_rank/get_total.py
+++ hacker_rank/get_total.py
@@ -1,18 +1,12 @@
 
 
 def getTotalX(a, b):
-	print(a)
-	print(b)
-	i = 0
-	a_len = len(a)
-	b_len = len(b)
-
-	while 1 < a_len:
-		i += 1
-		el = a[i]
-		print(el)	
-		if i == a_len:
-			break
+	results=[]
+	for i in range(1,max(b)+1):
+		if all(i%anum==0 for anum in a) and all(bnum%i==0 for bnum in b):
+			results.append(i)	
+	return len(results)
 
 
 result = getTotalX([2, 3], [2, 4])
+print(result)
