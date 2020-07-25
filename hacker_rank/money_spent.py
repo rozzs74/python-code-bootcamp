dfd59f5bb0f27f46c35f82dcaccdf59bbc99e477
Royce <royce@cbo.me>
2020-07-25 09:40:59 +0800

    solve money spent problem

--- hacker_rank/money_spent.py
+++ hacker_rank/money_spent.py
@@ -1,18 +1,22 @@
 def getMoneySpent(keyboards, drives, total_money):
     k_length = len(keyboards)
-    d_length = len(drives)
     i = 0
-    while k_length > 1:
+    within_range = []
+    too_much = True
+    while k_length >= 0:
+        total = 0
         keyboard_price = keyboards[i]
-        drive_price = drives[i]
-        total = keyboard_price + drive_price
-        if total < total_money:
-            print(total)
+        for drive in drives:
+            total = keyboard_price + drive
+            if total <= total_money:
+                within_range.append(total)
+                too_much = False
         i += 1
         if i == k_length:
-            break
-    pass
-
+            return max(within_range) if too_much != True else -1
 a = getMoneySpent([40, 50, 60], [5, 8, 12], 60)
 
+# a = getMoneySpent([3, 1], [5, 2, 8], 10)
+# a = getMoneySpent([4], [5], 5)
+print(a)
 # 9
