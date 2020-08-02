90b0ce98dd78e73c1592631a08ca9bfdeb91fdd5
Royce <royce@cbo.me>
2020-08-02 11:38:54 +0800

    work on beautiful days

---
+++ hacker_rank/beautiful_days.py
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem?h_r=next-challenge&h_v=zen
+def beautifulDays(start, end, divisor):
+    count = 0
+    for i in range(start, end+1):
+        final = 0
+        num = str(i)
+        reversed_int  = int(num[::-1])
+        numerator = abs(i - reversed_int)
+        final = numerator // divisor
+        if numerator % divisor == 0:
+            count += 1
+    return count
+a = beautifulDays(1, 2000000, 23047885)
+print(a)
+# a = beautifulDays(123, 456789, 189)
+
+
