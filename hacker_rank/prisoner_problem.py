fcc51cb96885e3a6c5f194a3096cbb6c7bdc03ec
Royce <royce@cbo.me>
2020-08-04 19:56:20 +0800

    add prisoner problem

---
+++ hacker_rank/prisoner_problem.py
@@ -0,0 +1,4 @@
+def saveThePrisoner(n, m, s):
+    return (m-1+s)%n or n
+
+# https://www.hackerrank.com/challenges/save-the-prisoner/problem
--- hacker_rank/viral_advertising.py
+++ hacker_rank/viral_advertising.py
@@ -1,3 +1,8 @@
 # https://www.hackerrank.com/challenges/strange-advertising/problem?h_r=next-challenge&h_v=zen
 def viralAdvertising(n):
-
+    total = 2;
+    liked = 2;
+    for _ in range(n-1):
+        liked = liked * 3//2
+        total += liked
+    return total
