7e5bec1a5de95420e76d8a3c6fc9fde514edc18a
Royce <royce@cbo.me>
2020-08-08 10:18:46 +0800

    solve append delete problem

--- hacker_rank/append_and_delete.py
+++ hacker_rank/append_and_delete.py
@@ -1,21 +1,24 @@
 def appendAndDelete(s, t, k):
-	a = k // 2
-	print(a)
-	b = 0
-	for _ in range(0, k):
-		if _ + a == k:
-			b = _
-			break
-	s1 = s[:-b]
-	t1 = t[-a:]
-	fword = f"{s1}{t1}"
+    s_length = len(s)
+    t_length = len(t)
 
-    
-         
+    if s_length + t_length < k: return 'Yes'
+
+    same = 0
+    for s_l, t_l in zip(s, t):
+        if s_l == t_l: same += 1
+        else: break
+   
+    extra_s = s_length - same
+    extra_t = t_length - same
+    difference = extra_s + extra_t
+
+    if difference <= k and difference % 2 == k % 2: return 'Yes'
+    return 'No'
 
 
-
-
-a = appendAndDelete("hackerhappy", "hackerrank", 9)
-# a = appendAndDelete("ashley", "ash", 3)
+# a = appendAndDelete("hackerhappy", "hackerrank", 9)
+a = appendAndDelete("aba", "aba", 7)	
+print(a)
 # a = appendAndDelete("ashley", "ash", 2)
+
