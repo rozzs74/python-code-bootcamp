8333955794aed61e08baa23ea283eafedbc857be
John Royce <johnroyce@royce-macbook-pro.local>
2021-01-02 15:00:12 +0800

    solve day two

---
+++ hacker_rank/2021/January/2/jumping_clouds.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+
+
+def jumpingOnClouds(c):
+	if len(c) == 1 : return 0
+	if len(c) == 2: return 0 if c[1]==1 else 1
+	if c[2]==1: 
+		return 1 + jumpingOnClouds(c[1:])
+	if c[2]==0:
+		return 1 + jumpingOnClouds(c[2:])
+
+a = jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])
+print(a)
